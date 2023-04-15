9bb7c33eebbb78d2206842a1551dfaf2bb9fd00b
noamkesten <noamkesten@gmail.com>
2023-04-15 03:53:31 +0300

    added logo

--- client/my-app/src/components/NavBar.js
+++ client/my-app/src/components/NavBar.js
@@ -3,12 +3,15 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import logo from "../assets/bet31nba-logo.png";
 
 const NavBar = () => {
   const isLoggedin = localStorage.getItem("userID");
   return (
     <AppBar position="static">
       <Toolbar>
+        <Avatar sx={{ marginRight: 3 }} alt="Logo" src={logo} />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Bet31NBA
         </Typography>
